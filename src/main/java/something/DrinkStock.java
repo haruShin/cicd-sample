f5be42866bcc3abd1f00a69185b4a1db7f68ee14
江本龍二 <emotoryuuji@emotoryuujinoMacBook-Pro.local>
2019-11-29 13:36:20 +0900

    repair DrinkStock
    Equarlメソッドのオーバーライド追加

--- src/main/java/something/DrinkStock.java
+++ src/main/java/something/DrinkStock.java
@@ -12,18 +12,18 @@ public class DrinkStock {
         this.number = number;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        DrinkStock that = (DrinkStock) o;
-//        return price == that.price &&
-//                number == that.number &&
-//                Objects.equals(name, that.name);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name, price, number);
-//    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DrinkStock that = (DrinkStock) o;
+        return price == that.price &&
+                number == that.number &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, number);
+    }
 }
