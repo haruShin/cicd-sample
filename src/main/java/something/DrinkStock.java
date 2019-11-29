9651039b24c59f5cc9c4f358b7108a84788e4be7
江本龍二 <emotoryuuji@emotoryuujinoMacBook-Pro.local>
2019-11-29 10:49:58 +0900

    create java project

---
+++ src/main/java/something/DrinkStock.java
@@ -0,0 +1,29 @@
+package something;
+
+import java.util.Objects;
+
+public class DrinkStock {
+    public String name;
+    public int price;
+    public int number;
+    public DrinkStock(String name, int price, int number) {
+        this.name = name;
+        this.price = price;
+        this.number = number;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        DrinkStock that = (DrinkStock) o;
+//        return price == that.price &&
+//                number == that.number &&
+//                Objects.equals(name, that.name);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, price, number);
+//    }
+}
